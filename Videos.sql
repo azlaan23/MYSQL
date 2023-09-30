4bc8697e5e678c636934129206e2d0d779889d23
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2023-09-30 19:12:21 +0530

    Add files via upload

---
+++ Videos.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE  ist_lecture;
+USE ist_lecture;
+
+CREATE TABLE student (
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NULL
+);
+
+INSERT INTO student VALUES ( 1, "AZLAAN", 21);
+INSERT INTO student VALUES (2,"ALAM", 23 );
+
+SELECT *FROM student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
